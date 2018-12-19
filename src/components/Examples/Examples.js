29dab8ad36e76445a31bd6cf5f63e8071df652b2
Terence Mangram <tmangram@gmail.com>
2018-12-18 21:38:51 -0600

    Add some initial stuf

--- src/components/Examples/Examples.js
+++ src/components/Examples/Examples.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Image, Transformation } from 'cloudinary-react';
 // import { PropTypes } from 'prop-types';
 
-const Examples = () => <div>Hello</div>;
+const Examples = () => (
+  <div className="examples">
+    <h2 className="examples__header">See What You Can Create</h2>
+    <div className="examples__box">
+      <img src="http://placehold.jp/150x150.png" className="examples__item" alt="stuff" />
+      <img src="http://placehold.jp/150x150.png" className="examples__item" alt="stuff" />
+      <img src="http://placehold.jp/150x150.png" className="examples__item" alt="stuff" />
+      <img src="http://placehold.jp/150x150.png" className="examples__item" alt="stuff" />
+      <img src="http://placehold.jp/150x150.png" className="examples__item" alt="stuff" />
+      <img src="http://placehold.jp/150x150.png" className="examples__item" alt="stuff" />
+    </div>
+  </div>
+);
 
 // Examples.propTypes = {};
 
 export default Examples;
+
+/*
+
+<Image
+      cloudName="spotifive"
+      publicId="https://media.pitchfork.com/photos/5931a95c31bcdd12429285f0/2:1/w_790/3b06ba70.jpg"
+      type="fetch"
+    >
+      <Transformation width="300" height="300" radius="max" fetchFormat="auto" />
+    </Image>
+
+*/
