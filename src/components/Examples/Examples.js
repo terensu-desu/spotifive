a53c1946c60964a948927f4fe12a9fe6e3e6adeb
Terence Mangram <tmangram@gmail.com>
2018-12-18 20:47:45 -0600

    Init dirs of split components

---
+++ src/components/Examples/Examples.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { PropTypes } from 'prop-types';
+
+const Examples = () => <div>Hello</div>;
+
+// Examples.propTypes = {};
+
+export default Examples;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import { PropTypes } from 'prop-types';
+
+const Header = ({ accessToken, getAuth, getTops }) => {
+  let callToAction = (
+    <button type="button" className="btn__cta" name="spotifyLogin" onClick={getAuth}>
+      Sign in through Spotify
+    </button>
+  );
+  if (accessToken) {
+    callToAction = (
+      <Fragment>
+        <button type="button" className="btn__cta" name="artists" onClick={getTops}>
+          Get top artists
+        </button>
+        <button type="button" className="btn__cta" name="tracks" onClick={getTops}>
+          Get top tracks
+        </button>
+      </Fragment>
+    );
+  }
+  return (
+    <div className="header">
+      <h1 className="header__main">Spotifive</h1>
+      <h2 className="header__sub">Show the world what you're jamming to</h2>
+      <h3 className="header__author">Created by Terence Mangram</h3>
+      <div className="header__call-to-action">{callToAction}</div>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  getAuth: PropTypes.func.isRequired,
+  getTops: PropTypes.func.isRequired,
+  accessToken: PropTypes.string
+};
+
+export default Header;
