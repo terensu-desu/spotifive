cf610681c77156902149cb1bf6e44bb490652ff2
Terence Mangram <tmangram@gmail.com>
2018-12-18 20:47:27 -0600

    Make dir for config files, export one public file of keys

---
+++ src/config/keys.js
@@ -0,0 +1,6 @@
+const spotify = require('./spotify');
+
+module.exports = {
+  spotifyID: spotify.clientID,
+  spotifySecret: spotify.clientSecret
+};
---
+++ src/config/spotify.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientID: '031b2d60d693490aa6410b24574c5fcc',
+  clientSecret: '482ae29b59a74d04b85c9c2c3aa54d26'
+};
