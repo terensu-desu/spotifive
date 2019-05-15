a8f896c9da02d3b7ba9078fb12f8324f34155bf5
Terence Mangram <tmangram@gmail.com>
2019-05-14 23:19:34 -0500

    Add new footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <p className="footer__main">
+      <a href="https://github.com/terensu-desu/spotifive">Spotifive</a>
+    </p>
+    <p className="footer__author">
+      Created by <a href="https://www.linkedin.com/in/terence-mangram/">Terence Mangram</a>
+    </p>
+  </div>
+);
+
+export default Footer;
